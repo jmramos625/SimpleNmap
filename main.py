6cc665924b953f4882f2a359f4068119a2f16745
jmramos625 <jmramos625@gmail.com>
2022-06-29 20:56:38 -0400

    Implementação do dirb ao código do programa

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 def escanear(ip):
     os.system(f"nmap -A -p- -Pn {ip} -v")
+    os.system(f"dirb {ip}")  # implementação do DIRB
 
 
 escanear(input("Qual IP(s) gostaria de escanear: "))
